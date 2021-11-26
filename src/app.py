02ddbb1bff4849ff58351e7b77d1137f6900f21a
Pentti Lähdeaho <pentti.lahdeaho@gmail.com>
2021-11-26 19:30:56 +0200

    Initial commit

---
+++ src/app.py
@@ -0,0 +1,14 @@
+from flask import (
+    Flask,
+    render_template,
+)
+
+app = Flask(__name__)
+
+@app.route("/")
+def render_home():
+    return render_template("index.html")
+
+@app.route("/ping")
+def ping():
+    return "Pong"
