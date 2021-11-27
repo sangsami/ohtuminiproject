b57898d6530aab603f7c612e46b355b549eee8c9
Jan Rundt <jan.rundt@gmail.com>
2021-11-27 17:03:36 +0200

    check title and author

--- src/services/lukuvinkki_service.py
+++ src/services/lukuvinkki_service.py
@@ -4,11 +4,16 @@ from repositories.lukuvinkki_repository import lukuvinkki_repository as default_
 class LukuvinkkiExistsError(Exception):
     pass
 
+class LukuvinkkiTitleOrAuthor(Exception):
+    pass
+
 class LukuvinkkiService:
     def __init__(self, lukuvinkki_repository=default_lukuvinkki_repository):
         self._lukuvinkki_repository = lukuvinkki_repository
 
     def create_lukuvinkki(self, title, author, description, link, comment):
+        if len(title) == 0 or len(author) == 0:
+            raise LukuvinkkiTitleOrAuthor("Check that you have entered atleast a title and an author.")
         if self._lukuvinkki_repository.check_lukuvinkki(title, author):
             raise LukuvinkkiExistsError("The lukuvinkki already exists.")
         self._lukuvinkki_repository.create(Lukuvinkki(title, author, description, link, comment))
