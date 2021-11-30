9be497391fdaf76d3a3e0039b3a33e4006e6337d
Sangsami <dinh.sami@gmail.com>
2021-11-30 14:49:58 +0200

    pylint wrong import position fix

--- src/app.py
+++ src/app.py
@@ -3,4 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 app.secret_key = "this_will_need_to_be_changed_in_production"
 
+# pylint: disable=wrong-import-position
+
 import routes
+
+# pylint: disable=wrong-import-position
