8bfbcb2ecd6a2781da6212f3059edcb8a23ca16a
Pentti Lähdeaho <pentti.lahdeaho@gmail.com>
2021-12-05 23:28:46 +0200

    change model's password length

--- src/models/user.py
+++ src/models/user.py
@@ -5,8 +5,8 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(50))
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
     time_created = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
 
     def __repr__(self) -> str:
