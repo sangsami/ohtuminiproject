48c1d3c9967b25b6d82ef3d2c8c835e0ca08cc27
Pentti Lähdeaho <pentti.lahdeaho@gmail.com>
2021-12-05 22:12:16 +0200

    update login method

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -1,4 +1,12 @@
-from flask import Blueprint, render_template
+from flask import (
+    Blueprint, 
+    render_template, 
+    url_for, 
+    redirect, 
+    request,
+    flash
+)
+from models.user import User
 # import database, db
 
 auth = Blueprint('auth', __name__)
@@ -6,6 +14,21 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login')
 def login():
     return render_template('login.html')
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(username=username).first()
+    
+    # tässä tarkistetaan myös salasanan hash, kunhan register on tehty
+    # if not user or not check_password_hash(user.password, password)
+    if not user:
+        flash('Invalid username or password')
+        return redirect(url_for('auth.login'))
+
+    return redirect(url_for('render_home'))
 
 @auth.route('/logout')
 def logout():
