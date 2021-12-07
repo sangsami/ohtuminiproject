6e84b5d5cf6d7f8f1f9d8696b0694c54e434ee44
Pentti Lähdeaho <pentti.lahdeaho@gmail.com>
2021-12-07 17:47:47 +0200

    update register route

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -6,43 +6,28 @@ from flask import (
     request,
     flash
 )
+from auth.forms import RegistrationForm
 from werkzeug.security import generate_password_hash, check_password_hash
 from models.user import User
-from app import db
+
+from repositories.user_repository import user_repository
+from services.user_service import user_service
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/register')
-def register():
-    return render_template('register.html')
-
-@auth.route('/register', methods=['POST'])
+@auth.route('/register', methods=['GET', 'POST'])
 def register_post():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        new_user = user_service.create_user(form.username.data, form.password.data)
+        if new_user:
+            flash('Thanks for registering')
+            return redirect(url_for('auth.login'))
+        else:
+            flash('Username already in use')
+            return render_template('register.html', form=form)
 
-    username = request.form.get('username')
-    password = request.form.get('password')
-    password_confirmation = request.form.get('password_confirmation')
-
-    user = find_user_by_username(username)
-
-    errors = False
-    if user:
-        flash('Username already in use')
-        errors = True
-
-    if password != password_confirmation:
-        flash("Passwords don't match")
-        errors = True
-
-    if errors:
-        return redirect(url_for('auth.register'))
-
-    new_user = User(username=username, password=generate_password_hash(password, method='sha256'))
-
-    db.session.add(new_user)
-    db.session.commit()
-
-    return redirect(url_for('auth.login'))
+    return render_template('register.html', form=form)
 
 @auth.route('/login')
 def login():
@@ -53,10 +38,11 @@ def login_post():
 
     username = request.form.get('username')
     password = request.form.get('password') # pylint: disable=unused-variable
+    # form-validation
 
-    user = User.query.filter_by(username=username).first()
+    user = user_service.check_credentials(username, password)
 
-    if not user or not check_password_hash(user.password, password):
+    if not user:
         flash('Invalid username or password')
         return redirect(url_for('auth.login'))
 
@@ -66,5 +52,3 @@ def login_post():
 def logout():
     return 'Logout'
 
-def find_user_by_username(username):
-    return User.query.filter_by(username=username).first()
