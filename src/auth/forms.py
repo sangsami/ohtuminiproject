7dfe2c14a77256206fbf9e681564db66e81b6a11
Pentti Lähdeaho <pentti.lahdeaho@gmail.com>
2021-12-07 17:45:43 +0200

    add flaskWTF form for registration

---
+++ src/auth/forms.py
@@ -0,0 +1,23 @@
+from wtforms import Form, StringField, PasswordField, validators, ValidationError
+from services.user_service import user_service
+
+class RegistrationForm(Form):
+    username = StringField('Username', [
+        validators.Length(min=4, message='Username needs to be 4 characters long or longer'),
+        validators.DataRequired(message='Username is required'),
+        validators.Regexp('^[a-zA-Z]+$', message='Username must contain only letters from a to z')
+    ])
+
+    def validate_username(self, field):
+        if not user_service.check_if_unique_username(field.data):
+            self.username.errors += (ValidationError('Username already in use'),)
+
+    password = PasswordField('Password', [
+        validators.Length(min=8, message='Password needs to be 8 characters long or longer'),
+        validators.DataRequired(message='Password is required'),
+        validators.EqualTo('password_confirmation', message='Passwords must match')
+    ])
+
+    password_confirmation = PasswordField('Repeat Password', [
+        validators.DataRequired(message='Repeat password')
+    ])
