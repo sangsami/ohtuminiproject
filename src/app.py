ca2a060fc063784a14f3ceaee156666f6824383e
Pentti Lähdeaho <pentti.lahdeaho@gmail.com>
2021-12-08 23:41:22 +0200

    ignore wrong import position

--- src/app.py
+++ src/app.py
@@ -5,7 +5,6 @@ import fixforheroku
 from config import SECRET
 from repositories.user_repository import UserRepository
 from services.user_service import UserService
-from models.user import User
 
 app = Flask(__name__)
 app.secret_key = SECRET
@@ -14,6 +13,10 @@ app.config["SQLALCHEMY_DATABASE_URI"] = fixforheroku.uri
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+
+# pylint: disable=wrong-import-position
+from models.user import User
+# pylint: enable=unused-import
 
 user_repository = UserRepository(db, User)
 user_service = UserService(user_repository, User)
