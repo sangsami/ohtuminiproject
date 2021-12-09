3c7aec4dc2777e75efa9d4006742d6e1681f0d3f
Jan Rundt <jan.rundt@gmail.com>
2021-12-09 14:58:12 +0200

    pylint treshold

--- src/repositories/lukuvinkki_repository.py
+++ src/repositories/lukuvinkki_repository.py
@@ -1,21 +1,22 @@
 from entities.lukuvinkki import Lukuvinkki  # pylint: disable=unused-import
 from app import db
 
+
 class LukuvinkkiRepository:
     def __init__(self):
         self._lukuvinkkis = []
 
     def get_books(self):
-        return Lukuvinkki.query.filter(Lukuvinkki.current_type=="Book").order_by(Lukuvinkki.id.desc()).all()
+        return Lukuvinkki.query.filter(Lukuvinkki.current_type == "Book").order_by(Lukuvinkki.id.desc()).all()
 
     def get_blog_posts(self):
-        return Lukuvinkki.query.filter(Lukuvinkki.current_type=="Blog post").order_by(Lukuvinkki.id.desc()).all()
+        return Lukuvinkki.query.filter(Lukuvinkki.current_type == "Blog post").order_by(Lukuvinkki.id.desc()).all()
 
     def get_podcasts(self):
-        return Lukuvinkki.query.filter(Lukuvinkki.current_type=="Podcast").order_by(Lukuvinkki.id.desc()).all()
+        return Lukuvinkki.query.filter(Lukuvinkki.current_type == "Podcast").order_by(Lukuvinkki.id.desc()).all()
 
     def get_youtubes(self):
-        return Lukuvinkki.query.filter(Lukuvinkki.current_type=="Youtube").order_by(Lukuvinkki.id.desc()).all()
+        return Lukuvinkki.query.filter(Lukuvinkki.current_type == "Youtube").order_by(Lukuvinkki.id.desc()).all()
 
     def find_all(self):
         return Lukuvinkki.query.order_by(Lukuvinkki.id.desc()).all()
@@ -26,12 +27,12 @@ class LukuvinkkiRepository:
 
     def change_lukuvinkki(self, id, title, author, link, description, comment, type):
         lukuvinkki = self.get_lukuvinkki(id)
-        lukuvinkki.title = title,
-        lukuvinkki.author = author,
-        lukuvinkki.link = link,
-        lukuvinkki.descript = description,
-        lukuvinkki.comment = comment,
-        lukuvinkki.type = type,
+        lukuvinkki.title = title
+        lukuvinkki.author = author
+        lukuvinkki.link = link
+        lukuvinkki.descript = description
+        lukuvinkki.comment = comment
+        lukuvinkki.type = type
         db.session.commit()
 
     def check_lukuvinkki(self, title):
@@ -39,7 +40,7 @@ class LukuvinkkiRepository:
             if title == lukuvinkki.title:
                 return True
         return False
-    
+
     def get_lukuvinkki(self, id):
         return Lukuvinkki.query.get(id)
 
