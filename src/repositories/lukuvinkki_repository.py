d6d85a3bf290e7d3441a5e4c634d8f78305ca176
Sangsami <dinh.sami@gmail.com>
2021-12-10 13:04:30 +0200

    a little bit of repo tests

--- src/repositories/lukuvinkki_repository.py
+++ src/repositories/lukuvinkki_repository.py
@@ -4,7 +4,7 @@ from app import db
 # pylint: disable=line-too-long
 class LukuvinkkiRepository:
     def __init__(self):
-        self._lukuvinkkis = []
+        pass
 
     def get_books(self):
         return Lukuvinkki.query.filter(Lukuvinkki.lukuvinkki_type == "Book").order_by(Lukuvinkki.lukuvinkki_id.desc()).all()
@@ -44,19 +44,25 @@ class LukuvinkkiRepository:
         lukuvinkki.lukuvinkki_type = lukuvinkki_type
         db.session.commit()
 
+# pylint: disable=line-too-long
+
     def check_lukuvinkki(self, title):
-        for lukuvinkki in self._lukuvinkkis:
-            if title == lukuvinkki.title:
-                return True
+        query_title = Lukuvinkki.query.filter(Lukuvinkki.title == title).first()
+        if query_title:
+            return True
         return False
 
     def get_lukuvinkki(self, lukuvinkki_id):
         return Lukuvinkki.query.get(lukuvinkki_id)
 
+    def delete_lukuvinkki(self, lukuvinkki_id):
+        record_obj = Lukuvinkki.query.filter(Lukuvinkki.lukuvinkki_id==lukuvinkki_id).first()
+        db.session.delete(record_obj)
+        db.session.commit()
+
     def delete_all(self):
         self._lukuvinkkis = []
 
-# pylint: disable=line-too-long
     def example_db_ops(self):
         sql = "INSERT into test (username, password) VALUES (:username, :password);"
         db.session.execute(sql, {"username": "demo", "password": "no-crypto"})
